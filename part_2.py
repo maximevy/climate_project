ab0a7e212ee46f154d03e5814c9f10498bae186c
maximevy <maxime.vyncke@student.uclouvain.be>
2025-12-14 18:11:33 +0100

    result generation for part 2

--- part_2.py
+++ part_2.py
@@ -7,7 +7,12 @@ t_year = 200
 t_sec = t_year*365*24*60*60 
 t_tot = (0, t_sec) #Important : find the true timescales for computation
 
-T0 = 0  #initial temperature condition
+T0 = 0  #initial temperature condition [K]
+F =  0 #forcing [W*m**(-2)]
+
+# temp increase diff equation with fifth order and noise
+
+
 
 def dT_stoch(t, T):
     '''
@@ -15,11 +20,11 @@ def dT_stoch(t, T):
     caused by the radiative forcing.
     '''
     alpha = 0.058 #the feedback temperature dependence [W/m**2/K] 
-    beta = 4*10**(-6) # [W/m**2/K] 
+    beta = - 4*10**(-6) # [W/m**2/K] 
     lbda = - 0.88 #the slope of the top-of-atm flux N [W/m**2/K] 
-    F = 0 #W*m**{-2}5
     c = 8.36*10**8 #J*K**(-1)*m**(-2)
-    dT = 1/c*(F + lbda*T + alpha*T**2 + beta*T**5 + random.gauss(0, 10) )
+    noise = random.gauss(0, 10)
+    dT = 1/c*(F + lbda*T + alpha*T**2 + beta*T**5 + noise)
     return dT
 
 
@@ -29,7 +34,7 @@ sol = solve_ivp(fun = dT_stoch, t_span = t_tot, y0 = [T0], t_eval = np.linspace(
 
 
 plt.plot(sol.t/(365*24*60*60), sol.y[0])
-plt.title("Time series of the increase in global annual mean surface temperature ")
+plt.title(f"Time series of the increase in global annual mean surface temperature with a forcing of F = {F}")
 plt.xlabel("Time [years]")
 plt.ylabel("Temperature difference [K]")
 
