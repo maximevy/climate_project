fbe831f691a0be077b598eea1167a7a084b99083
Mathieu Henry <mathieu.henry@student.uclouvain.be>
2025-12-17 00:17:33 +0100

    génère figure 1a dans part1

--- part_1.py
+++ part_1.py
@@ -2,6 +2,74 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
+"""
+We solve the equations 1 and 2 of the assignment and reproduce the figures 1a, 1d, 2a and 2b 
+of Bloch-Johnson et al. (2015)
+"""
+
+# Constants 
+F2x = 3.71
+
+def Fnx(n):
+    return np.log2(n)*3.71
+
+# Figure 1a 
+lmbda = -0.88
+ac = -0.035
+am = 0.03
+ah = 0.058
+T0 = 287
+
+def N_T(a, b, c, start, stop, steps):
+    x = np.linspace(start - T0, stop - T0, num=steps)
+    y = a*x**2 + lmbda*x + c
+    roots = np.roots([a, b, c])
+    return [y, roots]
+
+def plot_1a(): # plots figure 1a of the paper 
+    x = np.linspace(285, 297, 100)
+    linear = N_T(0, lmbda, F2x, 285, 297, 100)
+    f_c = N_T(ac, lmbda, F2x, 285, 297, 100)
+    f_m = N_T(am, lmbda, F2x, 285, 297, 100) 
+    f_h = N_T(ah, lmbda, F2x, 285, 297, 100)
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, linear[0], linestyle = "--", color = "k")
+    ax.plot(x, f_c[0], color = "b")
+    ax.plot(x, f_m[0], color = "g")
+    ax.plot(x, f_h[0], color = "r")
+    
+    # plot slope of lambda
+    xmin, xmax = 286, 288
+    mask = (x >= xmin) & (x <= xmax)
+    ax.plot(x[mask], linear[0][mask], color = "m")
+    ax.text(287, 4, r"$\lambda$", color = "m")
+    
+    # text and annotations 
+    ax.text(286.75, -0.7, r"$T_0$")
+
+    
+    ax.set_xlabel("T (K)")
+    ax.set_ylabel("N (W/m²)")
+    ax.set_title("Figure 1a")
+    
+    ax.set_ylim(-2, 8)
+    ax.set_xlim(285, 297)
+    
+    ax.set_xticks([285, 287, 289, 291, 293, 295, 297]) # set ticks 
+    ax.axhline(0, color = "k") # add horizontal line at y=0 
+    for xi in [linear[1][-1], f_c[1][-1], f_m[1][-1]]: # add markers at roots on horizontal line 
+        ax.plot([xi + T0, xi + T0], [-0.15, 0.15], color='k')
+        ax.plot([287, 287], [-0.15, 0.15], color = "k")
+    
+    plt.tight_layout()
+    plt.show()
+ 
+plot_1a()
+
+
+
+
 
 # temp increase diff equation without fifth order and noise
 
