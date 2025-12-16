ceeff90843d76de39927f3e1fba0387e6b1b3edf
maximevy <maxime.vyncke@student.uclouvain.be>
2025-12-16 16:09:02 +0100

    adding small loop

--- part_2.py
+++ part_2.py
@@ -8,7 +8,7 @@ t_sec = t_year*365*24*60*60
 t_tot = (0, t_sec) #Important : find the true timescales for computation
 
 T0 = 0  #initial temperature condition [K]
-F =  0 #forcing [W*m**(-2)]
+F = 1.5 #forcing [W*m**(-2)]
 
 # temp increase diff equation with fifth order and noise
 
@@ -23,24 +23,33 @@ def dT_stoch(t, T):
     beta = - 4*10**(-6) # [W/m**2/K] 
     lbda = - 0.88 #the slope of the top-of-atm flux N [W/m**2/K] 
     c = 8.36*10**8 #J*K**(-1)*m**(-2)
-    noise = random.gauss(0, 10)
+    noise = random.gauss(0, 40) #normal is 10
     dT = 1/c*(F + lbda*T + alpha*T**2 + beta*T**5 + noise)
     return dT
 
 
-# print(dT_stoch(2))
 
-sol = solve_ivp(fun = dT_stoch, t_span = t_tot, y0 = [T0], t_eval = np.linspace(*t_tot, 20000))
-
-
-plt.plot(sol.t/(365*24*60*60), sol.y[0])
-plt.title(f"Time series of the increase in global annual mean surface temperature with a forcing of F = {F}")
-plt.xlabel("Time [years]")
-plt.ylabel("Temperature difference [K]")
+# sol = solve_ivp(fun = dT_stoch, t_span = t_tot, y0 = [T0], t_eval = np.linspace(*t_tot, 20000))
 
 
+# plt.plot(sol.t/(365*24*60*60), sol.y[0])
+# plt.title(f"Time series of the increase in global annual mean surface temperature with a forcing of F = {F}")
+# plt.xlabel("Time [years]")
+# plt.ylabel("Temperature difference [K]")
+
+#plot of 20 different solutions
+i = 0
+while i < 20:
+    sol = solve_ivp(fun = dT_stoch, t_span = t_tot, y0 = [T0], t_eval = np.linspace(*t_tot, 20000))
+    plt.plot(sol.t/(365*24*60*60), sol.y[0])
+    plt.title(f"Time series of the increase in global annual mean surface temperature with a forcing of F = {F}")
+    plt.xlabel("Time [years]")
+    plt.ylabel("Temperature difference [K]")
+    i += 1
 
 
+#Important thing to notice. System is nonlinear. When forcing around 3.5
+#there is a strong increase in the final temperature increase of the system.
 
 
 
