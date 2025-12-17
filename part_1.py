a59d36458fba0e62dd190d61a3aa50d43b64799d
Mathieu Henry <mathieu.henry@student.uclouvain.be>
2025-12-17 22:31:58 +0100

    finit figure 1d et commencé figure 2a

--- part_1.py
+++ part_1.py
@@ -10,6 +10,7 @@ of Bloch-Johnson et al. (2015)
 
 # Constants 
 F2x = 3.71
+T0 = 287
 
 def Fnx(n):
     return np.log2(n)*3.71
@@ -19,7 +20,7 @@ lmbda = -0.88
 ac = -0.035
 am = 0.03
 ah = 0.058
-T0 = 287
+
 
 def N_T(a, b, c, start, stop, steps):
     x = np.linspace(start - T0, stop - T0, num=steps)
@@ -27,13 +28,11 @@ def N_T(a, b, c, start, stop, steps):
     roots = np.roots([a, b, c])
     return [y, roots]
 
-def plot_1a(): # plots figure 1a of the paper 
+def fig_1a(): # plots figure 1a of the paper 
     # Data
     x = np.linspace(285, 297, 100)
     linear = N_T(0, lmbda, F2x, 285, 297, 100)
-    print(linear[1])
     f_c = N_T(ac, lmbda, F2x, 285, 297, 100)
-    print(f_c[1])
     f_m = N_T(am, lmbda, F2x, 285, 297, 100) 
     f_h = N_T(ah, lmbda, F2x, 285, 297, 100)
     
@@ -45,10 +44,10 @@ def plot_1a(): # plots figure 1a of the paper
     ax.plot(x, f_h[0], color = "r", label = r"$a_H=0.058, \Delta T_{{2x}}= ? $")
     
     # plot slope of lambda
-    xmin, xmax = 286, 288
+    xmin, xmax = 286.25, 287.75
     mask = (x >= xmin) & (x <= xmax)
-    ax.plot(x[mask], linear[0][mask], color = "m")
-    ax.text(287, 4, r"$\lambda$", color = "m")
+    ax.plot(x[mask], linear[0][mask], color = "magenta")
+    ax.text(287, 4, r"$\lambda$", color = "magenta")
     
     # text and arrows 
     ax.text(286.75, -0.7, r"$T_0$") # T0
@@ -62,7 +61,6 @@ def plot_1a(): # plots figure 1a of the paper
 )
     ax.annotate("", xy = (T0, 3.7), xytext = (T0, 0.2), 
                 arrowprops = dict(facecolor = "red", arrowstyle="->", lw=1))
-
 
     # labels 
     ax.set_xlabel("T (K)")
@@ -82,7 +80,6 @@ def plot_1a(): # plots figure 1a of the paper
 
     # legend
     handles, labels = ax.get_legend_handles_labels()
-    #print(handles, labels)
     proxy = Line2D([0], [0], color = "none", linestyle="")
     handles.insert(0, proxy)
     labels.insert(0, r"$\lambda=-0.88\ W/m^2/K$")
@@ -91,7 +88,111 @@ def plot_1a(): # plots figure 1a of the paper
     plt.tight_layout()
     plt.show()
  
-plot_1a()
+#fig_1a()
+
+# figure 1d
+lmbda = -1.28
+a_h = 0.058
+F4x = Fnx(4)
+
+def fig_1d():
+    # data 
+    x = np.linspace(286, 300, 100)
+    lin_2x = N_T(0, lmbda, F2x, 286, 300, 100)
+    quad_2x = N_T(a_h, lmbda, F2x, 286, 300, 100)
+    lin_4x = N_T(0, lmbda, F4x, 286, 300, 100)
+    quad_4x = N_T(a_h, lmbda, F4x, 286, 300, 100)
+    linF1 = N_T(0, lmbda, 0.5, 286, 300, 100)
+    quadF1 = N_T(a_h, lmbda, 0.5, 286, 300, 100)
+    linF2 = N_T(0, lmbda, 2, 286, 300, 100)
+    quadF2 = N_T(a_h, lmbda, 2, 286, 300, 100)  
+    
+    # plotting
+    fig, ax = plt.subplots()
+    ax.plot(x, lin_2x[0], linestyle = "--", color = "b", label = f"$2xCO_2\ (linear), \Delta T_{{2x}}={lin_2x[1][-1]:.1f}K$")
+    ax.plot(x, quad_2x[0], linestyle = "-", color = "b", label = f"$2xCO_2\ (quad), \Delta T_{{2x}}={quad_2x[1][-1]:.1f}K$")
+    ax.plot(x, lin_4x[0], linestyle = "--", color = "r", label = f"$4xCO_2\ (linear), \Delta T_{{4x}}={lin_4x[1][-1]:.1f}K$")
+    ax.plot(x, quad_4x[0], linestyle = "-", color = "r", label = r"$4xCO_2\ (quad), \Delta T_{{4x}}= ? $")
+    ax.plot(x, linF1[0], linestyle = "--", color = "k")
+    ax.plot(x, quadF1[0], linestyle = "-", color = "k")
+    ax.plot(x, linF2[0], linestyle = "--", color = "k")
+    ax.plot(x, quadF2[0], linestyle = "-", color = "k")
+    
+    # plot slope of lambda
+    xmin, xmax = 286.8, 287.2
+    mask = (x >= xmin) & (x <= xmax)
+    ax.plot(x[mask], linF2[0][mask], color = "magenta", lw = 2.5)
+
+    # labels 
+    ax.set_xlabel("T (K)")
+    ax.set_ylabel("N (W/m²)")
+    ax.set_title("Figure 1d")
+    
+    # annotations 
+    ax.axhline(0, color = "k") # add horizontal line at y=0 
+    ax.plot([287, 287], [-0.15, 0.15], color = "k") # small tick at T0
+    ax.text(T0, -1.5, r"$T_0$", ha = "center")
+    
+    # axis limits
+    ax.set_ylim(-7, 21)
+    
+    # legend
+    handles, labels = ax.get_legend_handles_labels()
+    proxy = Line2D([0], [0], color = "none", linestyle="")
+    for i in range(2):
+        handles.insert(0, proxy)
+    labels.insert(0, r"$a_H = 0.058\ W/m^2/K^2$")
+    labels.insert(0, r"$\lambda=-1.28\ W/m^2/K$")
+    
+    ax.legend(handles, labels, loc = "upper right")
+    
+    plt.tight_layout()
+    plt.show()
+        
+fig_1d()
+
+# figure 2 
+# 2a
+def delta_T_vs_a(lmbda, n):
+    """
+    Delta T as a function of values of a, for a given lambda and CO2 increase
+    
+    Parameters
+    ----------
+    lmbda : TYPE
+        DESCRIPTION.
+    n : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    delta_T : TYPE
+        DESCRIPTION.
+
+    """
+    F = Fnx(n)
+    delta_T = []
+    a_values = np.linspace(-0.10, 0.10, 100)
+    for a in a_values:
+        root = np.roots([a, lmbda, F])[-1]
+        if isinstance(root, (int, float)):
+            delta_T.append(root)
+        else: 
+            delta_T.append(False)
+    return delta_T
+
+delta_T_2x_vs_a_max = delta_T_vs_a(-0.79, 2)
+delta_T_2x_vs_a_mean = delta_T_vs_a(-1.17, 2)
+delta_T_2x_vs_a_min = delta_T_vs_a(-1.78, 2)
+delta_T_2x_vs_a_0 = delta_T_vs_a(0, 2)
+
+
+       
+    
+    
+
+
+
 
 
 
